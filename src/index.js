41db4a08dd421dee0fcf15549a3a617ec94d3a39
ashiii1 <aashishaik526@gmail.com>
2024-07-13 17:01:43 +0530

    layout cnged

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import { Provider } from 'react-redux';
+// import { store } from './store';
+
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(
+//   <Provider store={store}>
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   </Provider>
+// );
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
